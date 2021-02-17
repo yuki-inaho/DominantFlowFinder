be143e9ac368b4d6b1ca9311d0cbf73922be5377
yuki-inaho <yoshikawa@inaho.co>
2021-02-17 21:59:59 +0900

    Add indices converting lines

--- include/CuvatureExtremaFinder.h
+++ include/CuvatureExtremaFinder.h
@@ -23,8 +23,8 @@ public:
         cv::parallel_for_(cv::Range(0, _image_size.width * _image_size.height), [&](const cv::Range &range) {
             for (size_t r = range.start; r < range.end; r++)
             {
-                int32_t y = r / _image_size.width;
-                int32_t x = r % _image_size.height;
+                size_t y = r / _image_size.width;
+                size_t x = r % _image_size.height;
                 Position2D pos_2d = {x, y};
                 size_t hash = position2hash(pos_2d, _image_size);
                 Position2D pos_neighbor_highest_curvature = _search_relative_high_curvature_position(pos_2d);
@@ -39,19 +39,18 @@ public:
 
     void set_curvature_order_information()
     {
-        // TODO: use bimap
         std::vector<std::size_t> indices_sorted_by_curvature = argsort(_pixel_node_list);
 
-
-        for(size_t index = 0; index < indices_sorted_by_curvature.size(); index++){
-            _map_standard_index_to_curvarture_order_index.insert(
-                std::pair<StandardIndex, CurvatureOrderIndex>(index, indices_sorted_by_curvature[index])                
-            );
-            _map_curvarture_order_index_to_standard_index.insert(
-                std::pair<CurvatureOrderIndex, StandardIndex>(indices_sorted_by_curvature[index], index)
-            );
-        }
-
+        // TODO: rethink process flow
+        _map_standard_index_to_curvarture_order_index = std::vector<StandardIndex>(_image_size.width * _image_size.height, 0);
+        _map_curvarture_order_index_to_standard_index = std::vector<CurvatureOrderIndex>(_image_size.width * _image_size.height, 0);
+        cv::parallel_for_(cv::Range(0, _image_size.width * _image_size.height), [&](const cv::Range &range) {
+            for (size_t r = range.start; r < range.end; r++)            
+            {
+                _map_standard_index_to_curvarture_order_index[r] = indices_sorted_by_curvature[r];
+                _map_standard_index_to_curvarture_order_index[indices_sorted_by_curvature[r]] = r;
+            }
+        });
 
 
         // TODO: write test code with below lines
@@ -72,8 +71,8 @@ public:
 private:
     void _set_image_size(const cv::Mat &image)
     {
-        int32_t image_height = image.rows;
-        int32_t image_width = image.cols;
+        size_t image_height = image.rows;
+        size_t image_width = image.cols;
         _image_size = {image_width, image_height};
         std::cout << "<input image size> " << _image_size << std::endl;
     }
@@ -102,9 +101,9 @@ private:
 
     std::vector<PixelNode> _pixel_node_list;
 
-    // TODO: 
-    std::unordered_map<StandardIndex, CurvatureOrderIndex> _map_standard_index_to_curvarture_order_index;
-    std::unordered_map<CurvatureOrderIndex, StandardIndex> _map_curvarture_order_index_to_standard_index;
+    // TODO: rethink data structure
+    std::vector<CurvatureOrderIndex> _map_standard_index_to_curvarture_order_index;
+    std::vector<StandardIndex> _map_curvarture_order_index_to_standard_index;
     cv::Mat _curvature_image;
     ImageSize _image_size;
 };
