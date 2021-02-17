e5a31c2888e78376625ee8af0dc8dfccc8a92fb4
yuki-inaho <yoshikawa@inaho.co>
2021-02-17 16:25:52 +0900

    Add pragma once

--- include/convert.h
+++ include/convert.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "struct.h"
 
 size_t position2hash(Position2D pos_2d, ImageSize image_size)
--- include/operator.h
+++ include/operator.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <opencv2/opencv.hpp>
 
 cv::Mat elem_mul(const cv::Mat &mat_a, const cv::Mat &mat_b)
